43c90347f5aea10be813b3629b00f0c9615d40bb
rinkimi <razieldelok@mail.ru>
2017-05-30 16:10:27 +0300

    New version 0_queue

---
+++ 0_queue/0_queue_v3.py
@@ -0,0 +1,198 @@
+from tkinter import *
+from tkinter import ttk
+import sqlite3
+from subprocess import Popen
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class App(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.root = parent
+        self.ip0 = StringVar()
+        self.ip1 = StringVar()
+        self.inode = StringVar()
+        self.apc = StringVar()
+        self.name = StringVar()
+        self.tems = 'C:/Program Files (x86)/SICK/TEMS Manager/Ectn.Tems.TemsManager.exe'
+        self.driver = webdriver.Firefox()
+        self.gui()
+
+    def traffic_spot(self, event, ip):
+        addr = 'http://' + ip + ':8088'
+        self.driver.get(addr)
+        email_field = self.driver.find_element_by_name("username")
+        email_field.send_keys("operator")
+        password_field = self.driver.find_element_by_name("password")
+        password_field.send_keys("operator")
+        password_field.send_keys(Keys.RETURN)
+
+    def traffic_spot_xml(self, event, ip):
+        addr = 'http://' + ip + ':2077'
+        self.driver.get(addr)
+
+    def traffic_spot_ftp(self, event, ip):
+        addr = 'http://' + ip + ':8888'
+        self.driver.get(addr)
+
+    def exit_button(self):
+        try:
+            self.p1.kill()
+            self.p2.kill()
+        finally:
+            print('Done!')
+
+    def OnDouble(self, event):
+        widget = event.widget
+        selection = widget.curselection()
+        value = widget.get(selection[0])
+        self.sql(value[1])
+
+    def sql(self, num):
+        params = "SELECT cont, cont2, iNode, APC, name FROM queue WHERE num = " + str(num)
+        conn = sqlite3.connect('c:/bases/queue.db')
+        cur = conn.cursor()
+        cur.execute(params)
+        res = cur.fetchone()
+        conn.close()
+        self.name.set(res[4])
+        if res[1] is '0':
+            self.ip0.set(res[0])
+            self.ip0_global = res[0]
+            self.buttons()
+        else:
+            self.ip1.set(res[1])
+            self.ip0.set(res[0])
+            self.ip0_global = res[0]
+            self.ip1_global = res[1]
+            self.buttons2()
+
+    def buttons(self):
+        self.trafic1.config(state='active')
+        self.trafic2.config(state='disable')
+        self.trafic1_xml.config(state='active')
+        self.trafic2_xml.config(state='disable')
+        self.trafic1_ftp.config(state='active')
+        self.trafic2_ftp.config(state='disable')
+        self.ssh0.config(state='active')
+        self.ssh1.config(state='disable')
+        self.tems0.config(state='active')
+        self.tems1.config(state='disable')
+        self.ssh0.bind("<Button-1>", lambda event: self.ssh_connect(event, self.ip0_global))
+        self.tems0.bind("<Button-1>", lambda event: self.lazors(event, self.ip0_global))
+        self.trafic1.bind("<Button-1>", lambda event: self.traffic_spot(event, self.ip0_global))
+        self.trafic1_xml.bind("<Button-1>", lambda event: self.traffic_spot_xml(event, self.ip0_global))
+        self.trafic1_ftp.bind("<Button-1>", lambda event: self.traffic_spot_ftp(event, self.ip0_global))
+
+    def buttons2(self):
+        self.trafic1.config(state='active')
+        self.trafic2.config(state='active')
+        self.trafic1_xml.config(state='active')
+        self.trafic2_xml.config(state='active')
+        self.trafic1_ftp.config(state='active')
+        self.trafic2_ftp.config(state='active')
+        self.ssh0.config(state='active')
+        self.ssh1.config(state='active')
+        self.tems0.config(state='active')
+        self.tems1.config(state='active')
+        self.ssh0.bind("<Button-1>", lambda event: self.ssh_connect(event, self.ip0_global))
+        self.tems0.bind("<Button-1>", lambda event: self.lazors(event, self.ip0_global))
+        self.ssh1.bind("<Button-1>", lambda event: self.ssh_connect(event, self.ip1_global))
+        self.tems1.bind("<Button-1>", lambda event: self.lazors(event, self.ip1_global))
+        self.trafic1.bind("<Button-1>", lambda event: self.traffic_spot(event, self.ip0_global))
+        self.trafic2.bind("<Button-1>", lambda event: self.traffic_spot(event, self.ip1_global))
+        self.trafic1_xml.bind("<Button-1>", lambda event: self.traffic_spot_xml(event, self.ip0_global))
+        self.trafic2_xml.bind("<Button-1>", lambda event: self.traffic_spot_xml(event, self.ip1_global))
+        self.trafic1_ftp.bind("<Button-1>", lambda event: self.traffic_spot_ftp(event, self.ip0_global))
+        self.trafic2_ftp.bind("<Button-1>", lambda event: self.traffic_spot_ftp(event, self.ip1_global))
+
+    def ssh_connect(self, event, ip):
+        key = 'c:/Keys/key.ppk'
+        ssh_conn = 'putty.exe -ssh -i ' + key + ' smihaylov@' + ip
+        self.ssh_conn = Popen(ssh_conn, shell=True)
+
+    def lazors(self, event, ip):
+        try:
+            self.p1.kill()
+            self.p2.kill()
+        finally:
+            key = 'c:/Keys/key.ppk'
+            s = 'plink.exe -L 59801:192.168.10.200:59801 -ssh ' + ip + ' -l smihaylov -i ' + key
+            self.p1 = Popen(s)
+            self.p2 = Popen(self.tems)
+
+    def listfill(self):
+        params = "SELECT name, num FROM queue"
+        conn = sqlite3.connect('c:/bases/queue.db')
+        cur = conn.cursor()
+        cur.execute(params)
+        res = cur.fetchall()
+        for i in res:
+            self.listbox.insert(END, i)
+        conn.close()
+
+    def gui(self):
+        content = ttk.Frame(root, padding=(3, 3, 12, 12))
+        self.frame = ttk.Frame(content, borderwidth=5, relief="groove", width=200, height=100)
+        name_lb = ttk.Label(self.frame, text='Название рамки:')
+        self.name_val = ttk.Label(self.frame, textvariable=self.name, foreground='blue')
+        ip_lb = ttk.Label(self.frame, text='IP адрес:', width=33)
+        self.ip_val1 = ttk.Label(self.frame, textvariable=self.ip0, foreground='blue')
+        self.ip_val2 = ttk.Label(self.frame, textvariable=self.ip1, foreground='blue')
+        tfframe = ttk.Labelframe(content, text='Trafficspot', borderwidth=5, width=200, height=100)
+        self.trafic1 = ttk.Button(tfframe, text='Trafickspot c1', state='disable', width=13)
+        self.trafic1_xml = ttk.Button(tfframe, text='Trafick_xml c1', state='disable', width=13)
+        self.trafic1_ftp = ttk.Button(tfframe, text='Trafick_ftp c1', state='disable', width=13)
+        self.trafic2 = ttk.Button(tfframe, text='Trafickspot c2', state='disable', width=13)
+        self.trafic2_xml = ttk.Button(tfframe, text='Trafick_xml c2', state='disable', width=13)
+        self.trafic2_ftp = ttk.Button(tfframe, text='Trafick_ftp c2', state='disable', width=13)
+        sshframe = ttk.Labelframe(content, text='SSH', borderwidth=5, width=200, height=50)
+        self.ssh0 = ttk.Button(sshframe, text='SSH controller 1', state='disable', width=20)
+        self.ssh1 = ttk.Button(sshframe, text='SSH controller 2', state='disable', width=20)
+        temsframe = ttk.Labelframe(content, text='TEMS', borderwidth=5, width=200, height=50)
+        self.tems0 = ttk.Button(temsframe, text='TEMS controller 1', state='disable', width=20)
+        self.tems1 = ttk.Button(temsframe, text='TEMS controller 2', state='disable', width=20)
+        self.off_button = ttk.Button(content, text='Disconnect', command=self.exit_button)
+        self.listbox = Listbox(content, relief="groove", selectmode=SINGLE)
+        self.listfill()
+        self.listbox.bind("<Double-Button-1>", self.OnDouble)
+
+        content.grid(column=0, row=0, sticky=(N, S, E, W))
+        self.frame.grid(column=0, row=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+        name_lb.grid(row=0, column=0, sticky=W)
+        self.name_val.grid(row=0, column=1, sticky=E)
+        ip_lb.grid(row=2, column=0, sticky=W)
+        self.ip_val1.grid(row=2, column=1, sticky=E)
+        self.ip_val2.grid(row=3, column=1, sticky=E)
+        tfframe.grid(row=3, column=0, columnspan=3, rowspan=2, sticky=(N, S, E, W))
+        self.trafic1.grid(row=0, column=0)
+        self.trafic1_xml.grid(row=0, column=1)
+        self.trafic1_ftp.grid(row=0, column=2)
+        self.trafic2.grid(row=1, column=0)
+        self.trafic2_xml.grid(row=1, column=1)
+        self.trafic2_ftp.grid(row=1, column=2)
+        sshframe.grid(row=5, column=0, columnspan=3, sticky=(N, S, E, W))
+        self.ssh0.grid(row=0, column=0, sticky=(W, E))
+        self.ssh1.grid(row=0, column=1, sticky=(W, E))
+        temsframe.grid(row=6, column=0, columnspan=3, sticky=(N, S, E, W))
+        self.tems0.grid(row=0, column=0, sticky=(W, E))
+        self.tems1.grid(row=0, column=1, sticky=(W, E))
+        self.off_button.grid(row=7, column=0, columnspan=4, sticky=(W, E))
+        self.listbox.grid(row=0, column=3, rowspan=7, sticky=(N, S, E, W))
+
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        content.columnconfigure(0, weight=3)
+        content.columnconfigure(1, weight=3)
+        content.columnconfigure(2, weight=3)
+        content.columnconfigure(3, weight=1)
+        content.columnconfigure(4, weight=1)
+        content.rowconfigure(1, weight=1)
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.title('Ports')
+    App(root)
+    root.mainloop()
